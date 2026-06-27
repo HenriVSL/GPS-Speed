69a51dd0607a1d556ca78cae477dd8421de0613c
Henri Lavikainen <henri.lavikainen@gmail.com>
2026-06-27 15:19:19 +0300

    Initial Commit

---
+++ GPS Speed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GPS Speed
+//
+//  Created by Henri Lavikainen on 27.6.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GPS Speed/GPS_SpeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPS_SpeedApp.swift
+//  GPS Speed
+//
+//  Created by Henri Lavikainen on 27.6.2026.
+//
+
+import SwiftUI
+
+@main
+struct GPS_SpeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
